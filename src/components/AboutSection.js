14ed9f9728fe5e28850233fed914a92678ddf81e
ammer-ahmad <ameer5ahmad@gmail.com>
2021-01-21 18:44:39 -0500

    Added a wave animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -6,6 +6,7 @@ import { About, Description, Image, Hide } from "../styles";
 //Framer Motion
 import { motion } from "framer-motion";
 import { titleAnim, fade, photoAnim } from "../animation";
+import Wave from "./Wave";
 
 const AboutSection = () => {
   return (
@@ -33,6 +34,7 @@ const AboutSection = () => {
       <Image>
         <motion.img variants={photoAnim} src={home1} alt="Guy with a camera" />
       </Image>
+      <Wave />
     </About>
   );
 };
