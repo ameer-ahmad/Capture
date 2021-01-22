89736fc296e974fa8bd39fd0bbc3094a1c7bbfe9
ammer-ahmad <ameer5ahmad@gmail.com>
2021-01-22 16:51:21 -0500

    Added animations for our work and contact us pages

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { About } from "../styles";
 import Toggle from "./Toggle";
 import { AnimateSharedLayout } from "framer-motion";
+import { useScroll } from "./useScroll";
+import { fade } from "../animation";
 
 const FaqSection = () => {
-  const [faqToggle, setFaqToggle] = useState(false);
+  const [element, controls] = useScroll();
   return (
-    <Faq>
+    <Faq variants={fade} ref={element} animate={controls} initial="hidden">
       <h2>
         Any questions <span>FAQ</span>
       </h2>
