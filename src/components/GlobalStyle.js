fa61a21635fdf69ae846fabfbdd060aba0f29553
ammer-ahmad <ameer5ahmad@gmail.com>
2021-01-22 17:13:33 -0500

    mobile responsiveness

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -12,6 +12,13 @@ const GlobalStyle = createGlobalStyle`
         font-family: 'Inter', sans-serif;
         overflow-x: hidden;
     }
+
+    html {
+        @media (max-width: 1700px) {
+            font-size: 75%;
+        }
+
+    }
     button {
         font-family: 'Inter', sans-serif;
         font-weight: bold;
